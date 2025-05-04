32119d56c58fde8a3e1199fb07452d31a4b027cb
Hasan <hasan.elbadry2006@gmail.com>
2025-05-04 16:04:30 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ const App = () => (
               <Route path="/gallery" element={<Gallery />} />
               <Route path="/notes" element={<Notes />} />
               <Route path="/timeline" element={<Timeline />} />
-              <Route path="*" element={<NotFound />} />
+              <Route path="*" element={<Index />} />
             </Routes>
           </main>
           <Footer />
